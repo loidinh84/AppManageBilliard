6ad6027fed1c85b5d9e104f98cf6a5b5f845de2d
Thành Lợi <dinhhoangloibt@gmail.com>
2026-01-03 03:25:20 +0700

    tableManager

--- AppManageBilliard/UserControls/ucTableManager.xaml.cs
+++ AppManageBilliard/UserControls/ucTableManager.xaml.cs
@@ -1,28 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AppManageBilliard.BUS;
+using AppManageBilliard.DTO;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AppManageBilliard.UserControls
 {
-    /// <summary>
-    /// Interaction logic for ucTableManager.xaml
-    /// </summary>
     public partial class ucTableManager : UserControl
     {
         public ucTableManager()
         {
             InitializeComponent();
+            LoadTableList();
+        }
+
+        void LoadTableList()
+        {
+            if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            {
+                var tables = TableBUS.Instance.LoadTableList();
+                icTableList.ItemsSource = new ObservableCollection<AppManageBilliard.DTO.Table>(tables);
+            }
+        }
+
+        private void btnTable_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var table = button.Tag as AppManageBilliard.DTO.Table;
+
+            if (table != null)
+            {
+                MessageBox.Show($"Bạn đã chọn {table.Name} - Trạng thái: {table.Status}");
+            }
         }
     }
 }
